fdb02dab34bcaf4697dfe3ad773a417d96b9b0f9
Joshua <hello@joshuarocks.me>
2025-05-25 10:56:53 -0400

    fix: adds title to the site, in addition to meta descriptions for SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,10 @@
 	let { children } = $props();
 </script>
 
-
+<svelte:head>
+	<title>Citations</title>
+	<meta name="description" content="Generate MLA 8 citations easily" />
+</svelte:head>
 <div class="min-h-screen bg-white dark:bg-slate-600">
 {@render children()}
 </div>
